bdd9e5207f38959bc4c3c0fe483fd3a168fad666
Nathan Arthur <narthur@preemptive.com>
2018-10-05 13:16:55 -0400

    Reformat to comply with standardjs; no functional change

--- puzzle.js
+++ puzzle.js
@@ -1,62 +1,59 @@
-"use strict";
-var util = require('util');
+'use strict'
 var solver = require('./solver.js')
 
-var boardSize = 56;
+var boardSize = 56
 var basePieces = [
-    [32,11],
-    [32,10],
-    [28,14],
-    [28,7],
-    [28,6],
-    [21,18],
-    [21,18],
-    [21,14],
-    [21,14],
-    [17,14],
-    [14,4],
-    [10,7]
-];
+  [32, 11],
+  [32, 10],
+  [28, 14],
+  [28, 7],
+  [28, 6],
+  [21, 18],
+  [21, 18],
+  [21, 14],
+  [21, 14],
+  [17, 14],
+  [14, 4],
+  [10, 7]
+]
 
-
-function main() {
-    solver.init(boardSize, basePieces);
-    solver.findSolution(showProgress, solved);
+function main () {
+  solver.init(boardSize, basePieces)
+  solver.findSolution(showProgress, solved)
 }
 
-var UPDATE_COUNT = 100000;
-var counter = 0;
-var start = new Date().getTime();
-function showProgress(pieces, board) {
-	if (++counter % UPDATE_COUNT == 0) {
-        process.stdout.write('.');
-	}
+var UPDATE_COUNT = 100000
+var counter = 0
+var start = new Date().getTime()
+function showProgress (pieces, board) {
+  if (++counter % UPDATE_COUNT === 0) {
+    process.stdout.write('.')
+  }
 }
 
-function solved(pieces, board) {
-    printBoard(board);
-    process.stdout.write(pieces + "\n");
-    process.stdout.write((new Date().getTime() - start) / 1000  + " seconds");
-    process.stdout.write("\n\nSUCCESS!!!\n\n");
-    return true;
+function solved (pieces, board) {
+  printBoard(board)
+  process.stdout.write(pieces + '\n')
+  process.stdout.write((new Date().getTime() - start) / 1000 + ' seconds')
+  process.stdout.write('\n\nSUCCESS!!!\n\n')
+  return true
 }
 
-function printBoard(board) {
-    process.stdout.write("\n");
-    for (var y = 0; y < boardSize; y++) {
-        for (var x = 0; x < boardSize; x++) {
-            if (x > 0 && x < boardSize - 1 && y > 0 && y < boardSize - 1 && 0 == board[x][y]) {
-                process.stdout.write(" ");
-            } else {
-                process.stdout.write("" + board[x][y] % 10);
-            }
-            process.stdout.write(" ");
-        }
-        process.stdout.write("\n");
+function printBoard (board) {
+  process.stdout.write('\n')
+  for (var y = 0; y < boardSize; y++) {
+    for (var x = 0; x < boardSize; x++) {
+      if (x > 0 && x < boardSize - 1 && y > 0 && y < boardSize - 1 && board[x][y] === 0) {
+        process.stdout.write(' ')
+      } else {
+        process.stdout.write('' + board[x][y] % 10)
+      }
+      process.stdout.write(' ')
     }
-    process.stdout.write("\n");
+    process.stdout.write('\n')
+  }
+  process.stdout.write('\n')
 }
 
-main();
-process.stdout.write("\n");
-
+main()
+process.stdout.write('\n')
--- solver.js
+++ solver.js
@@ -1,118 +1,116 @@
-var boardSize;
-var pieces;
-var board;
-var placed;
+var boardSize
+var pieces
+var board
+var placed
 
-exports.init = function(size, basePieces) {
-    boardSize = size;
-    pieces = basePieces;
-    board = [];
-    for (var x = 0; x < size; x++) {
-        board[x] = [];
-        for (var y = 0; y < size; y++) {
-            board[x][y] = 0;
-        }
+exports.init = function (size, basePieces) {
+  boardSize = size
+  pieces = basePieces
+  board = []
+  for (var x = 0; x < size; x++) {
+    board[x] = []
+    for (var y = 0; y < size; y++) {
+      board[x][y] = 0
     }
-    placed = [];
-    for (var i = 0; i < pieces.length; i++) {
-        placed[i] = false;
+  }
+  placed = []
+  for (var i = 0; i < pieces.length; i++) {
+    placed[i] = false
+  }
+}
+
+exports.findSolution = function (progress, done) {
+  fillBoard(0, 0, 0, progress, done)
+}
+
+function fillBoard (piecesIndex, x, y, progress, done) {
+  for (var i = 0; i < pieces.length; i++) {
+    if (!placed[i] && checkAndPlaceBothWays(i, x, y, progress, done)) {
+      return true
     }
+  }
+  return false
 }
 
-exports.findSolution = function(progress, done) {
-    fillBoard(0, 0, 0, progress, done);
+function checkAndPlaceBothWays (piecesIndex, x, y, progress, done) {
+  return checkAndPlaceOneWay(piecesIndex, false, x, y, progress, done) ||
+        checkAndPlaceOneWay(piecesIndex, true, x, y, progress, done)
 }
 
-function fillBoard(piecesIndex, x, y, progress, done) {
-    for (var i = 0; i < pieces.length; i++) {
-        if (!placed[i] && checkAndPlaceBothWays(i, x, y, progress, done)) {
-            return true;
-        }
-    }
-    return false;
-}
+function checkAndPlaceOneWay (piecesIndex, rotated, x, y, progress, done) {
+  var piece = pieces[piecesIndex]
+  var width = rotated ? piece[1] : piece[0]
+  var height = rotated ? piece[0] : piece[1]
 
-function checkAndPlaceBothWays(piecesIndex, x, y, progress, done) {
-
-    return checkAndPlaceOneWay(piecesIndex, false, x, y, progress, done)
-        || checkAndPlaceOneWay(piecesIndex, true, x, y, progress, done);
-}
-
-function checkAndPlaceOneWay(piecesIndex, rotated, x, y, progress, done) {
-    var piece = pieces[piecesIndex];
-    var width = rotated ? piece[1] : piece[0];
-    var height = rotated ? piece[0] : piece[1];
-
-    if (checkAndPlace(x, y, width, height, piecesIndex + 1)) {
-        placed[piecesIndex] = true;
-        if (allPlaced()) {
-            if (done(pieces, board)) {
-                return true;
-            }
-        } else {
-            if (findNextCorner(x, y, done, function(x, y) {
-                return fillBoard(piecesIndex + 1, x, y, progress, done);
-            })) {
-                return true;
-            }
-        }
-        unPlace(x, y, width, height);
-        placed[piecesIndex] = false;
+  if (checkAndPlace(x, y, width, height, piecesIndex + 1)) {
+    placed[piecesIndex] = true
+    if (allPlaced()) {
+      if (done(pieces, board)) {
+        return true
+      }
     } else {
-        progress(pieces, board);
+      if (findNextCorner(x, y, done, function (x, y) {
+        return fillBoard(piecesIndex + 1, x, y, progress, done)
+      })) {
+        return true
+      }
     }
-    return false;
+    unPlace(x, y, width, height)
+    placed[piecesIndex] = false
+  } else {
+    progress(pieces, board)
+  }
+  return false
 }
 
-function findNextCorner(startX, startY, done, callback) {
-    for (var y = startY; y < boardSize; y++) {
-        for (var x = (y == startY ? startX : 0); x < boardSize; x++) {
-            if (!board[x][y]) {
-                return callback(x, y);
-            }
+function findNextCorner (startX, startY, done, callback) {
+  for (var y = startY; y < boardSize; y++) {
+    for (var x = (y === startY ? startX : 0); x < boardSize; x++) {
+      if (!board[x][y]) {
+        return callback(x, y)
+      }
+    }
+  }
+  return false
+}
+
+function checkAndPlace (x, y, width, height, value) {
+  if (x + width > boardSize || y + height > boardSize) return false
+
+  // check the corners first
+  if (board[x + width - 1][y] || board[x][y + height - 1] || board[x + width - 1][y + height - 1]) return false
+
+  for (var i = x; i < x + width; i++) {
+    for (var j = y; j < y + height; j++) {
+      if (!board[i][j]) {
+        board[i][j] = value
+      } else {
+        // oops! something's here; better clean up our mess...
+        for (var l = x; l <= i; l++) {
+          for (var m = y; m < (i === l ? j : y + height); m++) {
+            board[l][m] = 0
+          }
         }
+        return false
+      }
     }
-    return false;
+  }
+  return true
 }
 
-function checkAndPlace(x, y, width, height, value) {
-    if (x + width > boardSize || y + height > boardSize) return false;
-
-    //check the corners first
-    if (board[x+width-1][y] || board[x][y+height-1] || board[x+width-1][y+height-1]) return false;
-
-    for (var i = x; i < x + width; i++) {
-        for (var j = y; j < y + height; j++) {
-            if (!board[i][j]) {
-                board[i][j] = value;
-            } else {
-                //oops! something's here; better clean up our mess...
-                for (var l = x; l <= i; l++) {
-                    for (var m = y; m < (i == l ? j : y + height); m++) {
-                        board[l][m] = 0;
-                    }
-                }
-                return false;
-            }
-        }
+function unPlace (x, y, width, height) {
+  for (var i = x; i < x + width; i++) {
+    for (var j = y; j < y + height; j++) {
+      board[i][j] = 0
     }
-    return true;
+  }
 }
 
-function unPlace(x, y, width, height) {
-    for (var i = x; i < x + width; i++) {
-        for (var j = y; j < y + height; j++) {
-            board[i][j] = 0;
-        }
+function allPlaced () {
+  for (var i = 0; i < placed.length; i++) {
+    if (!placed[i]) {
+      return false
     }
+  }
+  return true
 }
-
-function allPlaced() {
-    for (var i = 0; i < placed.length; i++) {
-        if (!placed[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
